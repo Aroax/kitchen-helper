2380ca2d18d34fdc4fd70844ef4ec9d09cee85b1
Arran Oakes <arran.oakes@gmail.com>
2021-07-30 08:24:42 +0100

    added edamum GET + add to pantry logic, but failing to store retrieved result to state correctly

--- KitchenHelperProject/src/main/js/components/pantry.js
+++ KitchenHelperProject/src/main/js/components/pantry.js
@@ -4,14 +4,20 @@ import axios from "axios";
 
 const pantry = (props) => {
   const [showForm, setShowForm] = useState(false);
+  const [storedIngredient, setStoredIngredient] = useState();
 
-  const [name, setName] = useState("");
-  const [foodCategory, setFoodCategory] = useState("");
-  const [location, setLocation] = useState("");
-  const [weight, setWeight] = useState("");
-  const [imageUrl, setImageUrl] = useState("");
+  const [foodId, setFoodId] = useState("899");
+  const [name, setName] = useState("test_ingredient");
+  const [foodCategory, setFoodCategory] = useState("test");
+  const [location, setLocation] = useState("unknown");
+  const [weight, setWeight] = useState("99");
+  const [imageUrl, setImageUrl] = useState("http://www.amityinternational.com/wp-content/uploads/2019/02/product-placeholder.jpg");
 
   const ingredientsList = props.user.pantry;
+  let lookupName;
+  let food_app_ID = "d3e7d692";
+  let food_app_key = "8147d1ff5bab97e50f29cc6c98459afd";
+  let food_api_url = "https://api.edamam.com/api/food-database/v2/parser"
 
   const getIngredients = (pantry) => {
     return pantry.map((ingredient) => {
@@ -47,24 +53,66 @@ const pantry = (props) => {
     setImageUrl(event.target.value);
   }
 
+  const handleLookupNameChange = (event) => {
+    event.preventDefault();
+    lookupName = event.target.value;
+  }
+
+  const addStoredToPantry = () => {
+    setFoodId(storedIngredient.foodId);
+    setName(storedIngredient.label);
+    setFoodCategory(storedIngredient.category);
+    setImageUrl(storedIngredient.image);
+    addToPantry();
+  }
+
   const addToPantry = () => {
+    event.preventDefault();
     axios({
       method: 'patch',
       url: `/users/${props.user.id}/pantry/add`,
       headers: { 'Content-Type': 'application/json' },
       data: {
-        foodId: '899',
+        foodId: foodId,
         name: name,
         foodCategory: foodCategory,
         location: location,
         weight: weight,
-        imageUrl: "http://www.amityinternational.com/wp-content/uploads/2019/02/product-placeholder.jpg",
+        imageUrl: imageUrl,
         expiry: new Date(),
       }
     }).then((response) => {
         console.log(response);
     })
     setTimeout(location.reload.bind(location), 3000);
+  }
+
+  const ingredientLookup = () => {
+    event.preventDefault();
+    axios({
+      method: 'get',
+      url: `${food_api_url}?app_id=${food_app_ID}&app_key=${food_app_key}&ingr=${lookupName}&nutrition-type=cooking`,
+      headers: { 'Content-Type': 'application/json' },
+    }).then((response) => {
+        // console.log(response);
+        // console.log(response.data);
+        // console.log(response.data.parsed);
+        // console.log(response.data.parsed[0]);
+        console.log(response.data.parsed[0].food);
+        setStoredIngredient(response.data.parsed[0].food.label);
+        console.log(storedIngredient);
+    });
+  }
+
+  const Lookup = () => {
+    return (
+      <div>
+        <form onSubmit={ingredientLookup}>
+          <input type="text" placeholder="Ingredient name" onChange={handleLookupNameChange}></input>
+          <input type="submit" value="search" />
+        </form>
+      </div>
+    )
   }
 
   const Form = () => {
@@ -79,13 +127,27 @@ const pantry = (props) => {
           <input type="hidden" placeholder="Image URL" onChange={handleImageUrlChange}></input>
         </form>
         <button onClick={addToPantry}>Add To Pantry</button>
+
       </div>
     )
   }
 
+  const LookupDisplayConfirmation = () => {
+    return(
+      <div>
+      <Ingredient data={storedIngredient} />
+      <button onClick={addStoredToPantry}>Add this ingredient to pantry</button>
+      </div>
+    )
+  }
+
+  // Render below
+
   return (
     <div className="container">
       {showForm ? <Form /> : null}
+      <p><Lookup /></p>
+      {storedIngredient ? <LookupDisplayConfirmation /> : <div>storage empty</div> }
       <button onClick={addIngredient}>Add Ingredient</button>
       <table>
         {getIngredients(ingredientsList)}
